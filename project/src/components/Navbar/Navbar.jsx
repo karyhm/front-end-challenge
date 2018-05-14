b4c2f13123f6cb51cf011a6b0560a073fdbbccd8
Kary <karina@arteko.mx>
2018-05-14 18:39:28 -0500

    SE DIVIDEN SECCIONES EN NAVBAR

--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
-import './Person.css'
-
+import style from './Navbar.css'
 const navbar = (props) => {
 	return (
-		<div className="Navbar">
-			<p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
-			<p>{props.children}</p>
-			<input type="text"	onChange={props.changed} value={props.name}/>	
-		</div>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<a className="navbar-brand" href="#">
+				<img src="" alt="Logo"/>
+			</a> | <span>EXCHANGE</span>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<ul className="navbar-nav">
+					<li className="nav-item active">
+						<a className="nav-link" href="#">1 BTC = 00000MXN</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Wallet</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Exchanghe</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Ayuda</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Usuario</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Switch</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
 	)
 }
 
