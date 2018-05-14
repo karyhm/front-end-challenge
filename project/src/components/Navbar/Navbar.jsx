9bf00648c938666e126a9560d92bab0e98a09b98
Kary <karina@arteko.mx>
2018-05-14 18:00:58 -0500

    COMPONENTE NAVBAR

---
+++ project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Person.css'
+
+const navbar = (props) => {
+	return (
+		<div className="Navbar">
+			<p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
+			<p>{props.children}</p>
+			<input type="text"	onChange={props.changed} value={props.name}/>	
+		</div>
+	)
+}
+
+export default navbar
