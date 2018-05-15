9375c6c0b97155065cd3d1ef4d2489a51911738b
Kary <karina@arteko.mx>
2018-05-15 11:31:36 -0500

    BACKGORUND COLOR TO NAVBAR COMPONENT AND ORDER LI ITEMS

--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -2,29 +2,63 @@ import React from 'react'
 import style from './Navbar.css'
 const navbar = (props) => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+		<nav class="navbar navbar-expand-lg">
 			<a className="navbar-brand" href="#">
-				<img src="" alt="Logo"/>
-			</a> | <span>EXCHANGE</span>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <img src="" alt="Logo" />
+			</a>
+			<button className="navbar-toggler" type="button"
+				data-toggle="collapse" data-target="#navbarNav"
+				aria-controls="navbarNav" aria-expanded="false"
+				aria-label="Toggle navigation">
 				<span className="navbar-toggler-icon"></span>
 			</button>
 			<div className="collapse navbar-collapse" id="navbarNav">
-				<ul className="navbar-nav">
+				<ul class="navbar-nav mr-auto">
+					<li className="divider">|</li>
+					<li className="nav-item active">
+						<a className="nav-link" href="#">EXCHANGE</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav">
 					<li className="nav-item active">
 						<a className="nav-link" href="#">1 BTC = 00000MXN</a>
 					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">Wallet</a>
+					<li className="divider">|</li>
+					<li className="nav-item dropdown">
+						<a className="nav-link dropdown-toggle"
+						data-toggle="dropdown" href="#" role="button" 
+						aria-haspopup="true" 
+						aria-expanded="false">
+							Wallet
+						</a>
+						<div className="dropdown-menu">
+							<a className="dropdown-item" href="#">Action</a>
+						</div>
 					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">Exchanghe</a>
+					<li className="nav-item dropdown">
+						<a className="nav-link dropdown-toggle"
+						data-toggle="dropdown" href="#" role="button" 
+						aria-haspopup="true" 
+						aria-expanded="false">
+							Exchange
+						</a>
+						<div className="dropdown-menu">
+							<a className="dropdown-item" href="#">Separated link</a>
+						</div>
 					</li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">Ayuda</a>
 					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">Usuario</a>
+					<li className="nav-item dropdown">
+						<a className="nav-link dropdown-toggle"
+						data-toggle="dropdown" href="#" role="button" 
+						aria-haspopup="true" 
+						aria-expanded="false">
+							Usuario
+						</a>
+						<div className="dropdown-menu">
+							<a className="dropdown-item" href="#">Action</a>
+						</div>
 					</li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">Switch</a>
