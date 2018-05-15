65de2648e9b554a7108be67f73ba8a65b6793dfe
Kary <karina@arteko.mx>
2018-05-15 11:33:35 -0500

    SUMMARY AND SIDEBAR ADDED TO APP.JS. SOME STYLES WERE ADDED AS WELL

--- project/src/App.js
+++ project/src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
+import Summary from './components/Summary/Summary';
+import SidebarLeft from './components/Sidebar/SidebarLeft';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 				<Navbar />
-        <header className="App-header">
+				<Summary />
+				<div className="row">
+					<div className="col col-md-3">
+						<SidebarLeft />
+					</div>
+					<div className="col md-auto">
+
+					</div>
+				</div>
+
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </header> */}
       </div>
     );
   }
