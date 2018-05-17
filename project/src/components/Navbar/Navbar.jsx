3dc4673adc2ec9bf526746852c5468d94d3faea9
Kary <karina@arteko.mx>
2018-05-17 11:03:52 -0500

    STYLES ADDED TO NAVBAR. NAVBAR IS COMPLETED

--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import style from './Navbar.css'
 const navbar = (props) => {
 	return (
-		<nav class="navbar navbar-expand-lg">
+		<nav className="navbar navbar-expand-lg">
 			<a className="navbar-brand" href="#">
 		    <img src="/img/1x/bitso_logo.png" alt="Logo" />
 			</a>
@@ -13,13 +13,13 @@ const navbar = (props) => {
 				<span className="navbar-toggler-icon"></span>
 			</button>
 			<div className="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav mr-auto">
+				<ul className="navbar-nav mr-auto">
 					<li className="divider"></li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">EXCHANGE</a>
 					</li>
 				</ul>
-				<ul class="navbar-nav">
+				<ul className="navbar-nav">
 					<li className="nav-item">
 						<a className="nav-link" href="#">1 BTC = 00000MXN</a>
 					</li>
@@ -27,7 +27,7 @@ const navbar = (props) => {
 					<li className="nav-item dropdown">
 						<a className="nav-link" href="#">
 							Wallet
-							<span class="caret"></span>
+							<span className="caret"></span>
 						</a>
 						<div className="dropdown-menu">
 							<a className="dropdown-item" href="#">Action</a>
@@ -36,19 +36,19 @@ const navbar = (props) => {
 					<li className="nav-item dropdown">
 						<a className="nav-link" href="#">
 							Exchange
-							<span class="caret"></span>
+							<span className="caret"></span>
 						</a>
 					</li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">Ayuda</a>
 					</li>
-					<li className="nav-item dropdown">
-						<div class="profile-img">
+					<li className="nav-item dropdown user-nav">
+						<div className="profile-img">
 							<img src="" alt=""/>
 						</div>
 						<a className="nav-link" href="#">
 							Usuario
-							<span class="caret"></span>
+							<span className="caret"></span>
 						</a>
 						<div className="dropdown-menu">
 							<a className="dropdown-item" href="#">Action</a>
@@ -56,9 +56,11 @@ const navbar = (props) => {
 					</li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">
-							<label class="switch">
+							<label className="switch">
 								<input type="checkbox" />
-								<span class="slider round"></span>
+								<span className="slider">
+									<div className="round"></div>
+								</span>
 							</label>
 						</a>
 					</li>
